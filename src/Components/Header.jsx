52217aa446c160815579a15917829489b784d774
jurgenstevens <jurgen.stevens@yahoo.com>
2022-09-29 16:03:01 -0500

    Add lorem to header.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <div>Header</div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, cum minima harum laborum consequatur deserunt illum in voluptates dicta vero similique amet repellat, dolorem quas sapiente. Excepturi perferendis ut praesentium.</p>
+    </>
   )
 }
 
